8fa9e873820151a2ef77bb472066af43b096337c
Josh <joshconstantinelim@gmail.com>
2023-09-10 17:36:11 +1000

    wk1

---
+++ wk1/listDelete.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+	int value;
+	struct node *next;
+};
+
+// delete the first instance of node with 'value' from the linked list
+// returns the head of the linked list after this is complete
+struct node *listDelete(struct node *list, int value) {
+	// TODO
+	return NULL;
+}
